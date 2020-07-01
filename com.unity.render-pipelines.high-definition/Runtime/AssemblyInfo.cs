f9afaf7e87a29596dcd7d8c49028c90574eb72ea
Robert Carone <racarone@gmail.com>
2020-07-01 15:51:21 +0100

    [SU] add internal assembilies

--- com.unity.render-pipelines.high-definition/Runtime/AssemblyInfo.cs
+++ com.unity.render-pipelines.high-definition/Runtime/AssemblyInfo.cs
@@ -3,5 +3,7 @@ using System.Runtime.CompilerServices;
 [assembly: InternalsVisibleTo("Unity.RenderPipelines.HighDefinition.Editor")]
 [assembly: InternalsVisibleTo("Unity.RenderPipelines.HighDefinition.Editor.Tests")]
 [assembly: InternalsVisibleTo("Unity.RenderPipelines.HighDefinition.Runtime.Tests")]
+[assembly: InternalsVisibleTo("SpeedUltra.Core.Runtime")]
+[assembly: InternalsVisibleTo("MagneticArcade.Graphics.Runtime")]
 [assembly: InternalsVisibleTo("HDRP_TestRunner")]
 
