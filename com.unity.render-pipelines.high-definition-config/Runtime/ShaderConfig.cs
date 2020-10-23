2c79587eba88ea2be85786c4ebd39899cff0ce24
Robert Carone <racarone@gmail.com>
2020-10-23 11:00:38 +0100

    [su] re-enable exposure

--- com.unity.render-pipelines.high-definition-config/Runtime/ShaderConfig.cs
+++ com.unity.render-pipelines.high-definition-config/Runtime/ShaderConfig.cs
@@ -25,7 +25,7 @@ namespace UnityEngine.Rendering.HighDefinition
     {
         CameraRelativeRendering = 1, // Rendering sets the origin of the world to the position of the primary (scene view) camera
         VertexFog = 0,
-        ExposureControl = 0, //[SU] Disable exposure (for perf)
+        ExposureControl = 1, //[SU] Disable exposure (for perf)
         PrecomputedAtmosphericAttenuation = 1, // Precomputes atmospheric attenuation for the directional light on the CPU, which makes it independent from the fragment's position, which is faster but wrong
 #if ENABLE_RAYTRACING
         Raytracing = 1,
